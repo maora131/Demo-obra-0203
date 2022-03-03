1a4038adf1a6dbd8f2e1de75c7a1c4cfa884133f
Maora <sofiasanmartin131@gmail.com>
2022-03-03 12:46:43 +0100

    Nuevas cosas
    Parte de citas/index y paginación.

--- citas/index.php
+++ citas/index.php
@@ -1,6 +1,17 @@
 <?php
 require '../conexion/conexion.php';
 require '../conexion/sesion.php';
+
+$where = "";
+
+if (!empty($_POST)) {
+  $valor = $_POST['campo'];
+  if (!empty($valor)) {
+    $where = "WHERE nome LIKE '%" . $valor . "%'";
+  }
+}
+$sql = "SELECT * FROM citas $where";
+$resultado = $mysqli->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +19,12 @@ require '../conexion/sesion.php';
 
 <head>
 
-  <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="./index.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+  <!-- <link rel="stylesheet" href="./index.css"> NO SE USA -->
+  <link rel="stylesheet" href="../assets/css/index.css">
   <script src="../components/js/header2.js"></script>
   <script src="../components/js/footer2.js"></script>
 
@@ -21,71 +34,90 @@ require '../conexion/sesion.php';
 
   <header-component></header-component>
 
-  <div class="master-container">
+  <div class="container">
 
-    <div class="container-resumo">
-      <h1>Axenda</h1>
+    <div class="row"></div>
+    <div class="row"></div>
 
-      <?php
-      $sql = "SELECT * FROM citas";
-      $resultado = $mysqli->query($sql);
+    <div class="row justify-content-center">
 
-      if ($resultado->num_rows > 0) {
-        echo "<table class='table'><tr><th>Resolto</th><th>Data</th><th>Nome</th><th>Hora de comezo</th><th>Hora de saída</th></tr>";
-        while ($row = $resultado->fetch_assoc()) {
-          echo "<tr><td><input type='checkbox'></form><td>" . $row["data"] . "</td><td>" . $row["nome"] . "</td><td>" . $row["horain"] . "</td><td>" . $row["horaout"] . "</td></tr>";
-        }
-        echo "</table>";
-      } else {
-        echo "0 resultados";
-      }
-
-      $mysqli->close();
-      ?>
-      <div class="row">
-        <a href="crear.php" class="btn btn-primary mb-4">Nova Cita</a>
-      </div>
-    </div>
-
-    <div class="container-notas">
-      <h2>Notas</h2>
-
-      <div class="list__items">
-        <ul></ul>
+      <div class="col col-sm-5">
+        <!-- <h3>CALENDARIO</h3> -->
+        <img src="../img/calendar.png" alt="calendar" class="mw-100">
       </div>
 
-      <div class="input__box">
-        <input type="text" name="" id="#input__text" placeholder="Nova nota">
-        <button class="add__btn"><i class="far fa-save fa-sm"></i></button>
-      </div>
+      <div class="col col-sm-7">
+        <!-- <h3 class="mt-2">AXENDA de CITAS</h3> -->
 
-      <div class="cat__details">
-        <span data-color="#f1c40f">
-          <p>Citas</p>
-          <div class="cat__type" style="background:#f1c40f;"></div>
-        </span>
-        <span data-color="#3498db">
-          <p>Documentación</p>
-          <div class="cat__type" style="background:#3498db;"></div>
-        </span>
-        <span data-color="#F94920">
-          <p>Reunións</p>
-          <div class="cat__type" style="background:#F94920;"></div>
-        </span>
-        <span data-color="#34495e">
-          <p>Chamadas</p>
-          <div class="cat__type" style="background:#34495e;"></div>
-        </span>
+        <div class="row table-responsive mt-5">
+
+          <table class="table table-striped table-hover mt-4">
+            <thead>
+              <tr>
+                <th class="col-sm-1">Resolto</th>
+                <th class="col-sm-2">Data</th>
+                <th class="col-sm-2">Nome</th>
+                <th class="col-sm-2">Hora comezo</th>
+                <th class="col-sm-2">Hora saída</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php while ($row = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
+                <tr>
+                  <td class="justify-content-center"><input type='checkbox'></td>
+                  <td><?php echo $row['data']; ?></td>
+                  <td><?php echo $row['nome']; ?></td>
+                  <td><?php echo $row['horain']; ?></td>
+                  <td><?php echo $row['horaout']; ?></td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
+
+        <button type="button" class="btn btn-light border"><a href="crear.php" class="text-decoration-none">Nova Cita</a></button>
       </div>
 
     </div>
+
+  </div>
   </div>
 
-  <script src="./index.js"></script>
-
   <footer-component></footer-component>
+
+  <script src="./citas.js"></script>
+
 </body>
 
+<!-- <div class="col col-sm-4">
+        <h3>NOTAS</h3>
+        <div class="list__items">
+          <ul></ul>
+        </div>
 
+        <div class="input__box">
+          <input type="text" name="" id="#input__text" placeholder="Nova nota">
+          <button class="add__btn"><i class="far fa-save fa-sm"></i></button>
+        </div>
+
+        <div class="cat__details">
+          <span data-color="#f1c40f">
+            <p>Citas</p>
+            <div class="cat__type" style="background:#f1c40f;"></div>
+          </span>
+          <span data-color="#3498db">
+            <p>Documentación</p>
+            <div class="cat__type" style="background:#3498db;"></div>
+          </span>
+          <span data-color="#F94920">
+            <p>Reunións</p>
+            <div class="cat__type" style="background:#F94920;"></div>
+          </span>
+          <span data-color="#34495e">
+            <p>Chamadas</p>
+            <div class="cat__type" style="background:#34495e;"></div>
+          </span>
+        </div>
+      </div> -->
 
 </html>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require 'conexion/sesion.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>OBRA 2 SOL</title>
   <!-- CND de bootstrap -> cambiar cuando toque -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="../assets/css/index.css">
   <script src="../components/js/header2.js"></script>
   <script src="../components/js/footer2.js"></script>
@@ -31,73 +31,58 @@ require 'conexion/sesion.php';
       <h1>Benvide á aplicación do Servizo de Orientación Laboral</h1>
     </div>
 
-
     <div class="card">
-              <!-- <div class="row g-0">
-                <div class="col-md-4">
-                  <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
 
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-              </div> -->
+      <div class="row">
+        <div class="col col-sm-8 d-flex flex-column justify-content-left">
+          <img src="../img/cies.jpg" width="650" height="400" alt="cies">
+        </div>
 
-    <div class="row">
-      <div class="col col-sm-8 d-flex flex-column justify-content-left">
-        <img src="../img/cies.jpg" width="650" height="400" alt="cies">
+        <div class="col col-sm-4 flex-column justify-content-right">
+          <div class="card-body mt-5">
+            <!-- <h4 class="card-title">Enlaces directos</h4> -->
+            <ul class="list-unstyled">
+              <li class="mt-3">
+                <h4 class="card-text">
+                  <a class="text-muted" href="https://sede.vigo.org/portal-empregado/#/gestionLogin"><img class="bi m-1" height="32" src="../../img/portal.png"></a>
+                  Portal do empregado
+                </h4>
+              </li>
+              <li class="mt-3">
+                <h4 class="card-text">
+                  <a class="text-muted" href="https://correo.vigo.org/zimbra/"><img class="bi m-1" height="32" src="../../img/peque-zimbra.png"></a>
+                  Correo Zimbra
+                </h4>
+              </li>
+              <li class="mt-3">
+                <h4 class="card-text">
+                  <a class="text-muted" href="https://hoxe.vigo.org/"><img class="bi m-1" height="32" src="../../img/escudo.png"></a>
+                  Concello de Vigo
+                </h4>
+              </li>
+              <li class="mt-3">
+                <h4 class="card-text">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-filetype-pdf m-1" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
+                  </svg>
+                  Manual do usuario
+                </h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+
       </div>
-
-      <div class="col col-sm-4 flex-column justify-content-right">
-      <div class="card-body mt-5">
-        <!-- <h4 class="card-title">Enlaces directos</h4> -->
-        <ul class="list-unstyled">
-          <li class="ms-3 mt-3">
-            <h4 class="card-text">
-              <a class="text-muted" href="https://sede.vigo.org/portal-empregado/#/gestionLogin"><img class="bi m-1" height="32" src="../../img/portal.png"></a>
-              Portal do empregado
-            </h4>
-          </li>
-          <li class="ms-3 mt-3">
-            <h4 class="card-text">
-              <a class="text-muted" href="https://correo.vigo.org/zimbra/"><img class="bi m-1" height="32" src="../../img/peque-zimbra.png"></a>
-              Correo Zimbra
-            </h4>
-          </li>
-          <li class="ms-3 mt-3">
-            <h4 class="card-text">
-              <a class="text-muted" href="https://hoxe.vigo.org/"><img class="bi m-1" height="32" src="../../img/escudo.png"></a>
-              Concello de Vigo
-            </h4>
-          </li>
-          <li class="ms-3 mt-3">
-            <h4 class="card-text">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-filetype-pdf m-1" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
-              </svg>
-              Manual do usuario
-            </h4>
-          </li>
-        </ul>
-      </div>
-      </div>
-
-    </div>
     </div>
 
-    <div class="row mt-5">
-    <div class="col col-sm-8">
-        <h3>Últimos avisos</h3>
+    <!-- <div class="row mt-5">
+      <div class="col col-sm-8">
+        <h4>Últimos avisos</h4>
         <table class="table table-striped table-bordered table-sm">
           <thead>
             <tr>
               <th class="col col-sm-2">Data</th>
               <th class="col col-sm-6">Aviso</th>
-              <!-- <th class="col col-sm-4">Enlace</th> -->
             </tr>
           </thead>
           <tbody>
@@ -120,7 +105,7 @@ require 'conexion/sesion.php';
           </tbody>
         </table>
       </div>
-    </div>
+    </div> -->
   </div>
 
   <!-- footer -->
